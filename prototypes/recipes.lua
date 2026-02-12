e5d0459d2d9a8afd4931ae1ff9ec37a78961c2fa
Adam Faulconbridge <afaulconbridge@googlemail.com>
2026-02-12 14:13:43 +0000

    Add simple recipes

---
+++ prototypes/recipes.lua
@@ -0,0 +1,82 @@
+-- prototypes/recipes.lua
+-- Recipes for the factorket mod
+
+local iron_to_coin = {
+  type = "recipe",
+  name = "iron-to-coin",
+  enabled = true,
+  ingredients = {
+    {type = "item", name = "iron-plate", amount = 1}
+  },
+  results = {
+    {type = "item", name = "coin", amount = 5}
+  },
+  energy_required = 0.5
+}
+
+local copper_to_coin = {
+  type = "recipe",
+  name = "copper-to-coin",
+  enabled = true,
+  ingredients = {
+    {type = "item", name = "copper-plate", amount = 1}
+  },
+  results = {
+    {type = "item", name = "coin", amount = 5}
+  },
+  energy_required = 0.5
+}
+
+local steel_to_coin = {
+  type = "recipe",
+  name = "steel-to-coin",
+  enabled = true,
+  ingredients = {
+    {type = "item", name = "steel-plate", amount = 1}
+  },
+  results = {
+    {type = "item", name = "coin", amount = 26}
+  },
+  energy_required = 0.5
+}
+
+local coin_to_yellow_belt = {
+  type = "recipe",
+  name = "coin-to-yellow-belt",
+  enabled = true,
+  ingredients = {
+    {type = "item", name = "coin", amount = 30}
+  },
+  results = {
+    {type = "item", name = "transport-belt", amount = 1}
+  },
+  energy_required = 0.5
+}
+
+local coin_to_yellow_splitter = {
+  type = "recipe",
+  name = "coin-to-yellow-splitter",
+  enabled = true,
+  ingredients = {
+    {type = "item", name = "coin", amount = 235}
+  },
+  results = {
+    {type = "item", name = "splitter", amount = 1}
+  },
+  energy_required = 0.5
+}
+
+local coin_to_yellow_underground = {
+  type = "recipe",
+  name = "coin-to-yellow-underground",
+  enabled = true,
+  ingredients = {
+    {type = "item", name = "coin", amount = 175}
+  },
+  results = {
+    {type = "item", name = "underground-belt", amount = 1}
+  },
+  energy_required = 0.5
+}
+
+data:extend({ iron_to_coin, copper_to_coin, steel_to_coin, coin_to_yellow_belt, coin_to_yellow_splitter, coin_to_yellow_underground })
