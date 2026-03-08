186c9b67097607b824d4028e078a56dff4c98cff
Adam Faulconbridge <afaulconbridge@googlemail.com>
2026-03-08 17:26:56 +0000

    update rich map preset

---
+++ prototypes/map-gen-presets.lua
@@ -0,0 +1,32 @@
+local controls = data.raw["map-gen-presets"]["default"]["rich-resources"].basic_settings.autoplace_controls
+controls["iron-ore"] = {
+    frequency = 1 / 6,
+    size = 2,
+    richness = 6
+}
+controls["copper-ore"] = {
+    frequency = 1 / 6,
+    size = 2,
+    richness = 6
+}
+controls["stone"] = {
+    frequency = 1 / 6,
+    size = 2,
+    richness = 6
+}
+controls["coal"] = {
+    frequency = 1 / 6,
+    size = 2,
+    richness = 6
+}
+controls["crude-oil"] = {
+    frequency = 1 / 6,
+    size = 2,
+    richness = 6
+}
+controls["uranium-ore"] = {
+    frequency = 1 / 6,
+    size = 2,
+    richness = 6
+}
+
