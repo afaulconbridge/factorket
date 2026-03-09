ef5710e83abe9d2c8a394df2e928ffb41fdd4854
Adam Faulconbridge <afaulconbridge@googlemail.com>
2026-03-09 17:41:31 +0000

    add more sales

--- prototypes/recipes-sell.lua
+++ prototypes/recipes-sell.lua
@@ -54,11 +54,83 @@ local steel_to_coin = {
     energy_required = 1
 }
 
--- TODO gears
--- TODO sticks
--- TODO green circuits
--- TODO red circuits
--- TODO blue circuits
--- TODO cables
+local iron_gear_wheel_to_coin = {
+    type = "recipe",
+    name = "iron-gear-wheel-to-coin",
+    category = "vending",
+    enabled = true,
+    ingredients = {{
+        type = "item",
+        name = "iron-gear-wheel",
+        amount = 10
+    }},
+    results = {{
+        type = "fluid",
+        name = "coin",
+        amount = 110
+    }},
+    energy_required = 1
+}
 
-data:extend({iron_to_coin, copper_to_coin, steel_to_coin})
+local iron_stick_to_coin = {
+    type = "recipe",
+    name = "iron-stick-to-coin",
+    category = "vending",
+    enabled = true,
+    ingredients = {{
+        type = "item",
+        name = "iron-stick",
+        amount = 10
+    }},
+    results = {{
+        type = "fluid",
+        name = "coin",
+        amount = 110
+    }},
+    energy_required = 1
+}
+
+local copper_cable_to_coin = {
+    type = "recipe",
+    name = "copper-cable-to-coin",
+    category = "vending",
+    enabled = true,
+    ingredients = {{
+        type = "item",
+        name = "copper-cable",
+        amount = 10
+    }},
+    results = {{
+        type = "fluid",
+        name = "coin",
+        amount = 27.5
+    }},
+    energy_required = 1
+}
+
+local electronic_circuit_to_coin = {
+    type = "recipe",
+    name = "electronic-circuit-to-coin",
+    category = "vending",
+    enabled = true,
+    ingredients = {{
+        type = "item",
+        name = "electronic-circuit",
+        amount = 10
+    }},
+    results = {{
+        type = "fluid",
+        name = "coin",
+        amount = 145.75
+    }},
+    energy_required = 1
+}
+
+-- TODO red circuits (plastic?)
+-- TODO blue circuits (acid?)
+-- TODO barelled oil and oil products
+-- TODO plastic
+-- TODO sulphur and barelled acid
+
+data:extend({iron_to_coin, copper_to_coin, steel_to_coin, iron_gear_wheel_to_coin, iron_stick_to_coin,
+             copper_cable_to_coin, electronic_circuit_to_coin})
